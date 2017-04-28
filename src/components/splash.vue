099be49d0d2af93be340b9f65f3b1227870fb9f4
Capi Etheriel <barraponto@gmail.com>
2017-04-27 22:49:17 -0300

    style splash component

--- src/components/splash.vue
+++ src/components/splash.vue
@@ -9,7 +9,7 @@
 </script>
 
 <template>
-  <div>
+  <main class="splash">
     <h1>
       A HISTÓRIA DA *RTE<router-link :to="{ name: 'about-index' }">[1]</router-link>
       é[2] a área do conhecimento[3] humano[4] que
@@ -20,8 +20,16 @@
       europeus[17], norte-americanos[18] e pintores[19] (alguns[20]) gênios[21]
       ...[22]
     </h1>
-  </div>
+  </main>
 </template>
 
 <style>
+  .splash {
+    box-sizing: border-box;
+    max-width: 768px;
+    padding: 1em;
+    font-family: 'Cabin', sans-serif;
+    line-height: 2.66em;
+    letter-spacing: 0.033em;
+  }
 </style>
